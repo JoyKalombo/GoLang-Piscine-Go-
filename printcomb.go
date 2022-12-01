499de1367ee6d960e4085998ff9890cc28cbdb74
jkalombo <joy__@hotmail.co.uk>
2022-12-01 16:41:25 +0000

    printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -6,12 +6,16 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				if i < j && j < k {
+				if i < j && j < k && !(i=='7' && j=='8' && k== '9'){
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					z01.PrintRune(',')
+				    z01.PrintRune(',')
 					z01.PrintRune(' ')
+				} else if i=='7' && j=='8' && k== '9'{
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 				}
 			}
 		}
