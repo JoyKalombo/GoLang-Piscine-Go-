ec2e9161a43ae834e450a004b8da3045e5b20e38
jkalombo <joy__@hotmail.co.uk>
2022-12-03 15:32:07 +0000

    .

--- strlen.go
+++ strlen.go
@@ -2,7 +2,7 @@ package piscine
 
 import "unicode/utf8"
 
-func StrLen(str string) int {
-	a := utf8.RuneCountInString(str)
+func StrLen(s string) int {
+	a := utf8.RuneCountInString(s)
 	return a
 }
