5d69f9affeeb3aef78fd7736936233c5f1332cd2
jkalombo <joy__@hotmail.co.uk>
2022-12-05 13:41:23 +0000

    factorials!

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,27 +1,27 @@
-package piscine
+// package piscine
 
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
 func IterativeFactorial(nb int) int {
 	result := 1
 
-	for i := 1; i <= nb; i++ {
-		if nb == 0 {
-			return 1
-		} else if nb < 0 {
-			return 0
-		} else {
+	if nb < 0 {
+		return 0
+	} else if nb == 0 {
+		return 1
+	} else {
+		for i := 1; i <= nb; i++ {
 			result = result * i
 		}
 	}
 	return result
 }
 
-// func main() {
-// 	arg := 4
-// 	fmt.Println(IterativeFactorial(arg))
-// }
+func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
+}
