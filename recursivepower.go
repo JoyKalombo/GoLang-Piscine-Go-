34c530e12037b3b425fc4e6f9e35553a8dd9f7ef
jkalombo <joy__@hotmail.co.uk>
2022-12-05 15:12:35 +0000

    power of recursion

--- recursivepower.go
+++ recursivepower.go
@@ -7,7 +7,7 @@ package piscine
 // )
 
 func RecursivePower(nb int, power int) int {
-	if nb == 0 || power < 0 {
+	if nb == 0 || power < 0 || (nb == 0 && power == 0) {
 		return 0
 	} else if nb == 1 || power == 0 {
 		return 1
