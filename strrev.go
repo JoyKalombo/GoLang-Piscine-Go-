f0581bd3de700ffbfef8e978c8c5340d6cf463a3
jkalombo <joy__@hotmail.co.uk>
2022-12-05 12:18:45 +0000

    reversing a string

---
+++ strrev.go
@@ -0,0 +1,21 @@
+package piscine
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+func StrRev(s string) string {
+	chars := []rune(s)
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
+}
+
+// func main() {
+// 	s := "Hello World!"
+// 	s = StrRev(s)
+// 	fmt.Println(s)
+// }
