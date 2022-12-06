42daeb310c319695539195481bed538197fdc0bf
jkalombo <joy__@hotmail.co.uk>
2022-12-06 15:19:45 +0000

    first letter or 0 index

---
+++ firstrune.go
@@ -0,0 +1,17 @@
+package piscine
+
+// package main
+
+// import "github.com/01-edu/z01"
+
+func FirstRune(s string) rune {
+	position := []rune(s)
+	return position[0]
+}
+
+// func main() {
+// 	z01.PrintRune(FirstRune("Hello!"))
+// 	z01.PrintRune(FirstRune("Salut!"))
+// 	z01.PrintRune(FirstRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }
