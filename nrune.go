8007e1fc4f329926c640f91d9f953597c68ba002
jkalombo <joy__@hotmail.co.uk>
2022-12-06 16:12:30 +0000

    printing the nth rune of a string

--- nrune.go
+++ nrune.go
@@ -1,17 +1,23 @@
-// package piscine
+package piscine
 
-package main
+// package main
 
-import "github.com/01-edu/z01"
+// import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	position := []rune(s)
-	return position[n-1]
+	if n >= 0 && n <= len(s) {
+		return position[n-1]
+	} else {
+		return 0
+	}
 }
 
-func main() {
-	z01.NRune(LastRune("Hello!"))
-	z01.NRune(LastRune("Salut!"))
-	z01.NRune(LastRune("Ola!"))
-	z01.NRune('\n')
-}
+// func main() {
+// 	z01.PrintRune(NRune("Hello!", 3))
+// 	z01.PrintRune(NRune("Salut!", 2))
+// 	z01.PrintRune(NRune("Bye!", -1))
+// 	z01.PrintRune(NRune("Bye!", 5))
+// 	z01.PrintRune(NRune("Ola!", 4))
+// 	z01.PrintRune('\n')
+// }
