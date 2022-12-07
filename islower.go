bef8f1b8de7662f75315ffbe3c43d4b13a547d6d
Joy Kalombo <jkalombo@noreply.learn.01founders.co>
2022-12-07 22:38:59 +0000

    Add 'islower.go'
    Right??

---
+++ islower.go
@@ -0,0 +1,19 @@
+// // package main
+
+package piscine
+
+// // import "fmt"
+
+func IsLower(s string) bool {
+	for _, j := range s {
+		if !(j >= 'a' && j <= 'z') {
+			return false
+		}
+	}
+	return true
+}
+
+// // func main() {
+// // 	fmt.Println(IsLower("hello"))
+// // 	fmt.Println(IsLower("hello!"))
+// // }
