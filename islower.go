efdf375acafa47ceea15cc85501696bc7e6395f8
jkalombo <joy__@hotmail.co.uk>
2022-12-07 22:17:54 +0000

    I have commented everything out here

--- islower.go
+++ islower.go
@@ -1,19 +1,19 @@
-// package main
+// // package main
 
-package piscine
+// package piscine
 
-// import "fmt"
+// // import "fmt"
 
 // func IsLower(s string) bool {
-	for _, j := range s {
-		if !(j >= 'a' && j <= 'z') {
-			return false
-		}
-	}
-	return true
-}
-
-// func main() {
-// 	fmt.Println(IsLower("hello"))
-// 	fmt.Println(IsLower("hello!"))
+// 	for _, j := range s {
+// 		if !(j >= 'a' && j <= 'z') {
+// 			return false
+// 		}
+// 	}
+// 	return true
 // }
+
+// // func main() {
+// // 	fmt.Println(IsLower("hello"))
+// // 	fmt.Println(IsLower("hello!"))
+// // }
