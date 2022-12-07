54f6b13dfe1c8989773017d69404cda941ef3e36
jkalombo <joy__@hotmail.co.uk>
2022-12-07 14:03:58 +0000

    is everything a capital letter???

---
+++ isupper.go
@@ -0,0 +1,19 @@
+package main
+
+// package piscine
+
+import "fmt"
+
+func IsUpper(s string) bool {
+	for _, j := range s {
+		if !(j >= 'A' && j <= 'Z') {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsUpper("HELLO"))
+	fmt.Println(IsUpper("HELLO!"))
+}
