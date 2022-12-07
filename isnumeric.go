1c6dffb8c71daffe97d0928fc7b622e4ae33e8b0
jkalombo <joy__@hotmail.co.uk>
2022-12-07 16:48:26 +0000

    we are checking that the string parsed only has numbers (spaces are not allowed)

---
+++ isnumeric.go
@@ -0,0 +1,19 @@
+package main
+
+// package piscine
+
+import "fmt"
+
+func IsNumeric(s string) bool {
+	for _, j := range s {
+		if !(j >= '0' && j <= '9') {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsNumeric("010203"))
+	fmt.Println(IsNumeric("01,02,03"))
+}
