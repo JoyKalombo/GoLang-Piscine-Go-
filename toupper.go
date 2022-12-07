3d64ccde3e4b4018eda8d44c293d0faa14f93017
jkalombo <joy__@hotmail.co.uk>
2022-12-07 22:44:42 +0000

    this will capitalise any lowercase letters

--- toupper.go
+++ toupper.go
@@ -6,19 +6,10 @@ package piscine
 // 	"fmt"
 // )
 
-func IsLower(s string) bool {
-	for _, j := range s {
-		if !(j >= 'a' && j <= 'z') {
-			return false
-		}
-	}
-	return true
-}
-
 func ToUpper(s string) string {
 	var storagestring string = ""
 	for _, i := range s {
-		if IsLower(string(i)) {
+		if i >= 'a' && i <= 'z' {
 			storagestring = storagestring + string(rune(i-32))
 		} else {
 			storagestring = storagestring + string(rune(i))
