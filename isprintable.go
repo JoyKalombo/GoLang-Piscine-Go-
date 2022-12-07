7fe7c983781e1de073a62fa7165ee9ed7eedbb61
jkalombo <joy__@hotmail.co.uk>
2022-12-07 18:55:38 +0000

    is it even printable???... We must know what it means for a character to be printable

---
+++ isprintable.go
@@ -0,0 +1,19 @@
+package main
+
+// package piscine
+
+import "fmt"
+
+func IsPrintable(s string) bool {
+	for _, j := range s {
+		if !(int(j) >= 20 && int(j) <= 127) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsPrintable("Hello"))
+	fmt.Println(IsPrintable("Hello\n"))
+}
