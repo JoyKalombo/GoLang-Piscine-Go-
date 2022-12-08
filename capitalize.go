fdb47d70a100bed60aedebe2129a374e3c9e198b
jkalombo <joy__@hotmail.co.uk>
2022-12-08 16:11:07 +0000

    This used a counter and concatenating to an empty list

--- capitalize.go
+++ capitalize.go
@@ -12,12 +12,15 @@ func Capitalize(s string) string {
 		if (i >= rune(32) && i <= rune(47)) || (i >= rune(58) && i <= rune(64)) || (i >= rune(91) && i <= rune(96)) || (i >= rune(123) && i <= rune(127)) {
 			storagestring = storagestring + string(rune(i))
 			count = 0
-		} else if count == 0 && (i >= 'A' && i <= 'Z') {
-			storagestring = storagestring + string(rune(i))
-			count++
 		} else if count == 0 && (i >= rune(33) && i <= rune(47)) || (i >= rune(58) && i <= rune(64)) || (i >= rune(91) && i <= rune(96)) || (i >= rune(123) && i <= rune(127)) {
 			storagestring = storagestring + string(rune(i))
 			count = 0
+		} else if count == 0 && (i >= 'A' && i <= 'Z') {
+			storagestring = storagestring + string(rune(i))
+			count++
+		} else if count == 0 && (i >= 'a' && i <= 'z') {
+			storagestring = storagestring + string(rune(i))
+			count++
 		} else if count != 0 && (i >= 'A' && i <= 'Z') {
 			storagestring = storagestring + string(rune(i+32))
 		} else {
