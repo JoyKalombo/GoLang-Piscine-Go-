d5af299bf0d61719326181170814d27afb93ba68
jkalombo <joy__@hotmail.co.uk>
2022-12-08 17:43:51 +0000

    example

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,8 +1,8 @@
-package main
+// package main
 
-// package piscine
+package piscine
 
-import "fmt"
+// import "fmt"
 
 func BasicAtoi2(s string) int {
 	num := 0
@@ -11,24 +11,22 @@ func BasicAtoi2(s string) int {
 	for _, char := range list {
 		if char >= '0' && char <= '9' {
 			for i := '0'; i < char; i++ {
-			 
 				count++
-				}
-			
+			}
+
 			num = num*10 + count
 			count = 0
 
 		} else {
 			return 0
 		}
-
 	}
 	return num
 }
 
-func main() {
-	fmt.Println(BasicAtoi2("12345"))
-	fmt.Println(BasicAtoi2("0000000012345"))
-	fmt.Println(BasicAtoi2("012 345"))
-	fmt.Println(BasicAtoi2("Hello World!"))
-}
+// func main() {
+// 	fmt.Println(BasicAtoi2("12345"))
+// 	// fmt.Println(BasicAtoi2("0000000012345"))
+// 	fmt.Println(BasicAtoi2("012 345"))
+// 	fmt.Println(BasicAtoi2("Hello World!"))
+// }
