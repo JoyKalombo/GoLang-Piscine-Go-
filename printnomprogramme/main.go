79de9f8112b52db3a6bd9c9b7dd4df7e803a9682
Joy Kalombo <jkalombo@noreply.learn.01founders.co>
2022-12-08 22:14:58 +0000

    Update 'printnomprogramme/main.go'

--- printnomprogramme/main.go
+++ printnomprogramme/main.go
@@ -10,7 +10,6 @@ func main() {
 	arguments := os.Args
 
 	for _, i := range arguments[0] {
-		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9') {
 			z01.PrintRune(i)
 		}
 	}
