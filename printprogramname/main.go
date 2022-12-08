3aeb1f8f0c9f0fcdb941a3c66054a9196faa4d87
jkalombo <joy__@hotmail.co.uk>
2022-12-08 19:26:19 +0000

    arguments??

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,8 +10,9 @@ func main() {
 	arguments := os.Args
 
 	for _, i := range arguments[0] {
-		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') {
+		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9') {
 			z01.PrintRune(i)
 		}
 	}
+	z01.PrintRune('\n')
 }
