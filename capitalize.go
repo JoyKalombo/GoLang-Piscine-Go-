856424bdf481e9f66474f30a38d621768ed68007
jkalombo <joy__@hotmail.co.uk>
2022-12-08 15:30:21 +0000

    This used a counter and concatenating to an empty list

---
+++ capitalize.go
@@ -0,0 +1,34 @@
+// package main
+package piscine
+
+// import "fmt"
+
+func Capitalize(s string) string {
+	var storagestring string = ""
+	count := 0
+
+	for _, i := range s {
+		if i == ' ' {
+			storagestring = storagestring + string(rune(i))
+			count = 0
+		} else if count == 0 && (i >= 'A' && i <= 'Z') {
+			storagestring = storagestring + string(rune(i))
+			count++
+		} else if count == 0 {
+			storagestring = storagestring + string(rune(i-32))
+			count++
+
+		} else if i >= 'A' && i <= 'Z' {
+			storagestring = storagestring + string(rune(i))
+		} else {
+			storagestring = storagestring + string(rune(i))
+			count++
+		}
+	}
+
+	return storagestring
+}
+
+// func main() {
+// 	fmt.Println(Capitalize("Hello! How are you? How+are+things+4you?"))
+// }
