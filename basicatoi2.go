1c6f6bba9ef1480918e06ef682ec52f0415f5e9a
jkalombo <joy__@hotmail.co.uk>
2022-12-08 17:59:52 +0000

    nuwio

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,8 +1,4 @@
-// package main
-
 package piscine
-
-// import "fmt"
 
 func BasicAtoi2(s string) int {
 	num := 0
@@ -23,10 +19,3 @@ func BasicAtoi2(s string) int {
 	}
 	return num
 }
-
-// func main() {
-// 	fmt.Println(BasicAtoi2("12345"))
-// 	// fmt.Println(BasicAtoi2("0000000012345"))
-// 	fmt.Println(BasicAtoi2("012 345"))
-// 	fmt.Println(BasicAtoi2("Hello World!"))
-// }
