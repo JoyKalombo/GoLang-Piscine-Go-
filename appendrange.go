61187ab5f2ec2ad44b4af8e5b11a682a2a0c4450
jkalombo <joy__@hotmail.co.uk>
2022-12-10 21:18:30 +0000

    slicing a list

--- appendrange.go
+++ appendrange.go
@@ -1,18 +1,18 @@
-package main
+// package main
 
-// package piscine
+package piscine
 
-import "fmt"
+// import "fmt"
 
 func AppendRange(min, max int) []int {
-	var array []int = []
-	
+	var array []int = nil
+
 	if max <= min {
 		array = nil
 	} else {
 		array = []int{}
 
-		for i := min; i <= max-1; i++ {
+		for i := min; i < max; i++ {
 			array = append(array, i)
 		}
 		return array
@@ -20,7 +20,7 @@ func AppendRange(min, max int) []int {
 	return nil
 }
 
-func main() {
-	fmt.Println(AppendRange(5, 10))
-	fmt.Println(AppendRange(10, 5))
-}
+// func main() {
+// 	fmt.Println(AppendRange(5, 10))
+// 	fmt.Println(AppendRange(10, 5))
+// }
