2bc404733eee6e24c5f8589efbc90c428c66950c
jkalombo <joy__@hotmail.co.uk>
2022-12-12 18:57:01 +0000

    This is slicing

--- makerange.go
+++ makerange.go
@@ -4,22 +4,17 @@ package piscine
 
 // import "fmt"
 
-
 func MakeRange(min, max int) []int {
-
 	if !(min < max) {
 		return nil
 	} else {
-		slice := make([]int, max - min)
+		slice := make([]int, max-min)
 		for i := 0; i < (max - min); i++ {
-			slice[i] = slice[min + i]
+			slice[i] = min + i
 		}
 		return slice
 	}
-
 }
-
-
 
 // func main() {
 // 	fmt.Println(MakeRange(5, 10))
