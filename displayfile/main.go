00703a5ae047d40c079deaaaa39f0a9c5adaa18c
Joy Kalombo <Joy__@hotmail.co.uk>
2022-12-14 13:14:09 +0000

    I should be able to see it now

---
+++ displayfile/main.go
@@ -0,0 +1,30 @@
+package main
+
+import 
+(
+	"fmt"
+	"os"
+)
+
+func main() {
+	if !(len(os.Args) <= 1) {
+		os.Args = os.Args[1:]
+	} else {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(os.Args) == 1 {
+		file, err := os.Open(os.Args[0])
+		if err != nil {
+			fmt.Printf("the mistake is : %v\n", err.Error())
+		} else {
+			arr := make([]byte, 78) //Am I looking at the correct file?
+			file.Read(arr)
+			fmt.Println(string(arr))
+			file.Close()
+		}
+	} else {
+		fmt.Println("Too many arguments")
+	}
+
+}
