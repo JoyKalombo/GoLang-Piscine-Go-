57f606d0770a8fa960591b16633b936302702717
Joy Kalombo <Joy__@hotmail.co.uk>
2022-12-14 19:10:59 +0000

    print used instead of println

--- displayfile/main.go
+++ displayfile/main.go
@@ -9,20 +9,20 @@ func main() {
 	if !(len(os.Args) <= 1) {
 		os.Args = os.Args[1:]
 	} else {
-		fmt.Print("File name missing")
+		fmt.Printf("File name missing")
 		return
 	}
 	if len(os.Args) == 1 {
 		file, err := os.Open(os.Args[0])
 		if err != nil {
-			fmt.Print("the mistake is : %v\n", err.Error())
+			fmt.Printf("the mistake is : %v\n", err.Error())
 		} else {
 			arr := make([]byte, 78)
 			file.Read(arr)
-			fmt.Print(string(arr))
+			fmt.Printf(string(arr))
 			file.Close()
 		}
 	} else {
-		fmt.Print("Too many arguments")
+		fmt.Printf("Too many arguments")
 	}
 }
