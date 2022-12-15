dd39ecfa36fbf5a24eb53cce6b1e72048c7446dc
Joy Kalombo <Joy__@hotmail.co.uk>
2022-12-15 00:28:02 +0000

    task on arguments

---
+++ cat/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		return
+	} else {
+		os.Args = os.Args[1:]
+
+		for _, i := range os.Args {
+			file, err := os.Open(i)
+			if err != nil {
+				fmt.Println(err)
+				return
+			} else {
+				data := make([]byte, 443)
+				file.Read(data)
+				if len(os.Args) == 1 {
+					fmt.Println(string(data))
+				} else {
+					fmt.Println(str(data), "\n")
+				}
+				file.Close()
+			}
+
+		}
+	}
+}
