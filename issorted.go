9063a02bd0004b2f48b906bee14f9b25807a7278
jkalombo <joy__@hotmail.co.uk>
2022-12-16 18:34:57 +0000

    for-loop fixed to account for all indices in the slice

--- issorted.go
+++ issorted.go
@@ -1,35 +1,42 @@
-package main
+// package main
 
-// package piscine
+package piscine
 
-import "fmt"
+// import "fmt"
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a) -1; i++ {
-		if f(a[i], a[i+1]) < 0 {
-			return true
-		} else if f(a[i], a[i+1]) > 0 {
-			return false
+	for i := 0; i < len(a); i++ {
+		if i < len(a) {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) > 0 {
+					return false
+				}
+			}
+		} else if i == len(a) {
+			if f(a[i-1], a[i]) > 0 {
+				return false
+			}
+		}
 	}
 	return true
 }
 
-func AscendingOrder(a,b int) int {
-	if a < b {
-		return -1
-	} else if a == b {
-		return 0
-	}
-	return 1
-}
+// func AscendingOrder(a, b int) int {
+// 	if a < b {
+// 		return -1
+// 	} else if a == b {
+// 		return 0
+// 	}
+// 	return 1
+// }
 
-func main() {
-	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{0, 2, 1, 3}
+// func main() {
+// 	a1 := []int{0, 1, 2, 3, 4, 5}
+// 	a2 := []int{0, 2, 1, 3}
 
-	result1 := IsSorted(AscendingOrder, a1)
-	result2 := IsSorted(AscendingOrder, a2)
+// 	result1 := IsSorted(AscendingOrder, a1)
+// 	result2 := IsSorted(AscendingOrder, a2)
 
-	fmt.Println(result1)
-	fmt.Println(result2)
-}
+// 	fmt.Println(result1)
+// 	fmt.Println(result2)
+// }
