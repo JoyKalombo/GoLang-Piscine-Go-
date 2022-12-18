0f3b76dc7e32dc3b71b5fc06696afe2fc6fbe2e0
jkalombo <joy__@hotmail.co.uk>
2022-12-18 21:25:15 +0000

    all 2 digit combinations written

---
+++ printcomb2.go
@@ -0,0 +1,25 @@
+// package main
+
+package piscine
+
+// import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			if !(i == '9' && j == '9') {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			} else {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+			}
+		}
+	}
+}
+
+// func main() {
+// 	PrintComb2()
+// }
