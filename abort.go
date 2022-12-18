3c66206433c04ef02ed833e33c1d36c3e328b2d0
jkalombo <joy__@hotmail.co.uk>
2022-12-18 19:41:04 +0000

    finding the median of 5 numbers

--- abort.go
+++ abort.go
@@ -9,7 +9,7 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 	arg := []int{a, b, c, d, e}
 	for _, i := range arg {
-		if i < len(arg)-1 {
+		if i >= 0 && i < len(arg)-1 {
 			if arg[i] > arg[i+1] {
 				arg[i], arg[i+1] = arg[i+1], arg[i]
 			}
