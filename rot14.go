2a97f0927c6b2dbe664ff9372b8b4b8125b472ed
jkalombo <joy__@hotmail.co.uk>
2022-12-18 14:34:34 +0000

    translating letters 14 spaces ahead (and cycling through the alphabet where necessary)

--- rot14.go
+++ rot14.go
@@ -5,16 +5,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func Rot14(s string) string {
+	emptystring := ""
 	for _, i := range s {
 		if (i >= rune(65) && i <= rune(76)) || (i >= rune(97) && i <= rune(108)) {
-			z01.PrintRune(i + 14)
+			emptystring += z01.PrintRune(i + 14)
 		} else if (i >= rune(77) && i <= rune(90)) || (i >= rune(109) && i <= rune(122)) {
-			z01.PrintRune(i + 14 - 26)
+			emptystring += z01.PrintRune(i + 14 - 26)
 		} else {
-			z01.PrintRune(i)
+			emptystring += z01.PrintRune(i)
 		}
 	}
-	return ""
+	return emptystring
 }
 
 // func main() {
