475e7f3cc7b87e3df4d509608074081814a32ee7
jkalombo <joy__@hotmail.co.uk>
2022-12-18 14:17:59 +0000

    translating letters 14 spaces ahead (and cycling through the alphabet where necessary)

---
+++ rot14.go
@@ -0,0 +1,24 @@
+package main
+
+// package piscine
+
+import "github.com/01-edu/z01"
+
+func Rot14(s string) string {
+	for _, i := range s {
+		if (i >= rune(65) && i <= rune(76)) || (i >= rune(97) && i <= rune(108)) {
+			z01.PrintRune(i + 14)
+		} else if (i >= rune(77) && i <= rune(90)) || (i >= rune(109) && i <= rune(122)) {
+			z01.PrintRune(i + 14 - 26)
+		}
+	}
+}
+
+func main() {
+	result := piscine.Rot14("Hello! How are You?")
+
+	for _, r := range result {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
